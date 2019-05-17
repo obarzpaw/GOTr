ba1764b60263e5063fd7e9ac86b31c442087a6f3
obarzpaw <pawelobarzanek@gmail.com>
2019-05-17 07:43:15 +0200

    Make CharacterData class parcelable

--- app/src/main/java/com/example/gotr/CharacterData.kt
+++ app/src/main/java/com/example/gotr/CharacterData.kt
@@ -1,12 +1,17 @@
 package com.example.gotr
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import kotlinx.serialization.Serializable
 
+
 @Serializable
-    data class Data(
+@Parcelize
+data class JsonData (
     val image: String = "https://vignette.wikia.nocookie.net/gameofthrones/images/d/d3/JonSnowSeason8HB.jpg/revision/latest/scale-to-width-down/333?cb=20190401173347",
     val name: String = "Jon Snow",
     val gender: String = "male",
     val culture: List<String> = listOf(
         "Northmen"
     )
-)
+) : Parcelable
+
